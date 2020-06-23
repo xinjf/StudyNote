c07609f1e9d560c7a5a349d77b90bb2b8b2a46dd
fengxinjian <770149839@qq.com>
2020-06-23 08:09:12 +0800

    choose_cases

---
+++ APIAuto/StudyNote/APIAuto_study/choose_cases.py
@@ -0,0 +1,17 @@
+import os
+import unittest
+
+
+def choose_all_cases(case_suite, pattern):
+    """
+    获取指定路径下所有的测试用例
+    :param
+    pattern: 匹配模式
+    case_suite: 执行的用例集的文件
+    cases_path: case的路径
+    :return: 测试用例集"""
+    case_suite = os.path.join(cases_path, case_suite)
+    discover_all_cases = unittest.defaultTestLoader.discover(case_suite, pattern=pattern,
+                                                             top_level_dir=None)
+    return discover_all_cases
+
