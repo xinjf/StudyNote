f21b4b1c893279ba55190e71619a15b409eab547
fengxinjian <770149839@qq.com>
2020-06-23 14:12:10 +0800

    Squashed commit of the following:
    commit bac87bc128dd988ae2336f52992442b3c83ef1f1
    Author: fengxinjian <770149839@qq.com>
    Date:   Tue Jun 23 13:50:39 2020 +0800
    
        20200623
    
    commit c07609f1e9d560c7a5a349d77b90bb2b8b2a46dd
    Author: fengxinjian <770149839@qq.com>
    Date:   Tue Jun 23 08:09:12 2020 +0800
    
        choose_cases
    
    commit e7de23132bca7609bbe718ca48c412ca917b1a7c
    Author: fengxinjian <770149839@qq.com>
    Date:   Sat Jun 20 16:46:54 2020 +0800
    
        优化run文件
    
    commit 03235707479f9e8043b85dc9bc92df0d34652e38
    Author: fengxinjian <770149839@qq.com>
    Date:   Sat Jun 20 14:23:15 2020 +0800
    
        connect mysql
    
    commit 98da4ab8d9d27d4a3c5d23b2199bded68eaad362
    Author: fengxinjian <770149839@qq.com>
    Date:   Thu Jun 18 18:54:17 2020 +0800
    
        ini的读写
    
    commit 88c6b26cf2cae861674ba7c5ca8798cbda7e0424
    Author: fengxinjian <770149839@qq.com>
    Date:   Thu Jun 18 16:16:11 2020 +0800
    
        11
    
    commit 3a4e6c57903c69872109711c045b1f25ece66826
    Author: fengxinjian <770149839@qq.com>
    Date:   Thu Jun 18 16:14:37 2020 +0800
    
        Squashed commit of the following:
    
        commit 352215c3e956f46e7542ab26e671e7b0f21f7ba7
        Author: fengxinjian <770149839@qq.com>
        Date:   Thu Jun 18 16:08:38 2020 +0800
    
            2020.06.18

--- APIAuto/StudyNote/APIAuto_study/read&write_ini.py
+++ APIAuto/StudyNote/APIAuto_study/read&write_ini.py
@@ -19,4 +19,37 @@ print(options)
 value = conf.get("title1", "key2")
 print(value)
 
+<<<<<<< HEAD
 
+=======
+# 返回title下所有的key，value
+print(conf.items("title1"))          # 返回列表包含元组
+print(dict(conf.items("title1")))    # 返回字典
+
+# 判断是否存在指定的title 或 value
+print(conf.has_section("title1"))
+print(conf.has_option("title1", "key1"))
+
+"""写入ini文件"""
+con = configparser.ConfigParser()
+con.read("./read&write.ini")
+
+# 添加title,并在title下加入新的key和value
+con.add_section("title3")
+con.set("title3", "key1", "1111")
+con.set("title3", "key3", "3333")
+
+# 移除指定title下的key和value
+con.remove_option("title3", "key3")
+
+# 移除指定的title
+con.remove_section("title1")
+
+# 返回title下所有的key，value
+print(con.items("title3"))          # 返回列表包含元组
+print(dict(con.items("title3")))    # 返回字典
+
+# 保存操作所做的修改
+with open("read&write.ini", "w+") as f:
+    con.write(f)
+>>>>>>> remotes/origin/2020.06.23
---
+++ APIAuto/StudyNote/APIAuto_study/time/time.py
@@ -0,0 +1,11 @@
+
+"""time模块"""
+import time
+# time.mktime()方法
+
+
+time.mktime(t)
+
+
+
+
