6926ea1f738ea83534ad6cf259bbdd1ef7bdf13a
xinjf <18884129577@163.com>
2020-07-22 09:29:35 +0800

    flask

---
+++ Flask/3、debug_mode.py
@@ -0,0 +1,10 @@
+"""调试模式"""
+# 打开调试模式，服务器会在修改应用代码之后自动重启，并且当应用出错时会提供一个有用的调试器
+# 需要打开所有开发功能(包括调试模式)，服务器之前导出FLASK_ENV环境变量并把设置为development
+
+
+# 进入应用所在的文件夹
+# $env:FLASK_APP="2、small_program.py"
+# set FLASK_ENV=development
+# flask run
+
